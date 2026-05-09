bce169e4c0e14a7670d1e0e0bac6ca008fe2fa74
terry.s <terry.s@kakaocorp.com>
2026-05-09 15:36:06 +0900

    feat: add rust template

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
           path = ./default;
           description = "A simple flake with empty dev shell";
         };
+
+        rust = {
+          path = ./rust;
+          description = "A simple flake with rust dev shell";
+        };
       };
       defaultTemplate = self.templates.default;
     };
---
+++ rust/flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:cpark97/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = forAllSystems (pkgs: {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            rustc
+            cargo
+            rust-analyzer
+            rustfmt
+          ];
+
+          RUST_SRC_PATH = "${pkgs.rust.packages.stable.rustPlatform.rustLibSrc}";
+        };
+      });
+    };
+
+}
